7ae71cb7424d4148e00c08ee5032872f17db4a4d
ndeans <ndeans@yahoo.com>
2017-11-11 21:33:23 -0800

    initial java webapp for SMM

---
+++ smm_proto/src/main/java/us/deans/xts/smm/XtsController.java
@@ -0,0 +1,33 @@
+package us.deans.xts.smm;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class XtsController
+ */
+public class XtsController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+    public XtsController() {
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ smm_proto/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<title>XTS Game System</title>
+</head>
+<body>
+<h2>XTS Game System</h2>
+</body>
+</html>
